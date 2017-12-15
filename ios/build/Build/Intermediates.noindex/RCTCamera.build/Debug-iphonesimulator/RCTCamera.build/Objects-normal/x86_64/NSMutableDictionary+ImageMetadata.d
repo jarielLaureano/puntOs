aed77d5b7bfc9e0d52100d00265de5ccc1763aa5
HectorAGarcia <hector.garcia@upr.edu>
2017-12-14 23:22:42 -0400

    Fixed build for ios

---
+++ ios/build/Build/Intermediates.noindex/RCTCamera.build/Debug-iphonesimulator/RCTCamera.build/Objects-normal/x86_64/NSMutableDictionary+ImageMetadata.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/hectorgarcia/Documents/Agosto2017/Capstone-2017/ReactProject/puntOs/node_modules/react-native-camera/ios/NSMutableDictionary+ImageMetadata.m
---
+++ ios/build/Build/Intermediates.noindex/RNFetchBlob.build/Debug-iphonesimulator/RNFetchBlob.build/Objects-normal/x86_64/RNFetchBlobProgress.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/hectorgarcia/Documents/Agosto2017/Capstone-2017/ReactProject/puntOs/node_modules/react-native-fetch-blob/ios/RNFetchBlobProgress.m \
+  /Users/hectorgarcia/Documents/Agosto2017/Capstone-2017/ReactProject/puntOs/node_modules/react-native-fetch-blob/ios/RNFetchBlobProgress.h
